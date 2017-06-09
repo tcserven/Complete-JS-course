a192ae6914788ecbe47df84dd5fe7c01163d7154
Thomas-Laptop <thomas.cservenyi@gmail.com>
2017-06-09 17:24:44 -0400

    more stuff

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -181,7 +181,12 @@ function retirement(retirementAge) {
 }
 
 var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
 retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
 
 // can also do this
 // retirement(66)(1990);
