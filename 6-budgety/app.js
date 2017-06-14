59bb3df2f929673d7588033429c4bb7a97afd546
Thomas-Laptop <thomas.cservenyi@gmail.com>
2017-06-14 18:49:45 -0400

    more on budgety

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -27,6 +27,63 @@
 // budget controller
 var budgetController = (function() {
 
+
+	// function constructors 	
+	var Expense = function(id, description, value) {
+		this.id = id,
+		this.description = description,
+		this.value = value
+	} 
+
+	var Income = function(id, description, value) {
+		this.id = id,
+		this.description = description,
+		this.value = value
+	} 
+
+
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	}
+
+	return {
+		addItem: function(type, des, val) {
+			var newItem, ID;
+
+			// [1, 2, 3, 4, 5], next ID should be 6
+			// [1 2 4 6 8], problem though cuz next one cant be 6, should be 9! 
+			// want ID to be last ID plus 1
+			if (data.allItems[type].length < 0) {
+				ID = data.allItems[type][data.allItems[type].length-1].id + 1; //plus 1 is for the NEW id
+			} else {
+				ID = 0;
+			}
+			
+
+			// remember, type refers to the select tag thats either a + or -
+			// create new item 
+			if (type === 'exp') {
+				newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			}
+
+			data.allItems[type].push(newItem);  //this line works because the exp or inc is the same as the allItems object and its two arrays
+			return newItem; //so that the other function thats going to call addItem can have direct access to the item we create 
+		},
+
+		testing: function() {
+			console.log(data);
+		}
+	}
+
 })();
 
 
@@ -79,11 +136,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 	
 
 	var ctrlAddItem = function() {
+		var input, newItem;
+
 		// 1. Get the field input data
-		var input = UIController.getInput();
-		console.log(input);
+		input = UIController.getInput();
+		// console.log(input);
 
 		// 2. Add the item to the budget controller
+		newItem = budgetController.addItem(input.type, input.description, input.value);
 
 		// 3. add the item to the UI
 
