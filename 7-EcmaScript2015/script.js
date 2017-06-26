ae766b66d2fb28da31d47a1c095965e1291b0835
Thomas <tcserven@uoguelph.ca>
2017-06-25 21:09:34 -0400

    finished course

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -135,7 +135,7 @@
 // 	color: 'green',
 // 	position: 1,
 // 	clickMe: function() {  //we want to attach an event handler to the green box
-		
+
 // 		document.querySelector('.green').addEventListener('click', () => {
 // 			var str = 'This is box ' + this.position + ' and it is ' + this.color;
 // 			alert(str);
@@ -149,7 +149,7 @@
 // 	position: 1,
 // 	clickMe: () => {  //we want to attach an event handler to the green box. 
 // 		// making clickMe into an arrow fnc doesnt work! it sets its this keyword to the global object. 
-		
+
 // 		document.querySelector('.green').addEventListener('click', () => {
 // 			var str = 'This is box ' + this.position + ' and it is ' + this.color;
 // 			alert(str);
@@ -207,24 +207,24 @@
 // Var john = [‘John’, 26];
 // Var name = john[0];
 // Var age = john[1];
- 
+
 // // es6
 // Const [name, age] = [‘John’, 26];
 // console.log(name);
 // console.log(age);
- 
+
 // Const obj = { firstName: ‘John’,  lastName: ‘Smith’  };
 // Const {firstName, lastName} = obj;
 // console.log(firstName);
 // console.log(lastName);
- 
+
 // Const {firstName: a, lastName: b} = obj;
 // console.log(a);
 // console.log(b);
- 
- 
+
+
 // Function calcAgeRetirement(year) {  const age = new Date().getFullYear() - year;  return [age, 65 - age] ; };
- 
+
 // Const [age, retirement] = calcAgeRetirement(1990);
 // console.log(age);
 // console.log(retirement);
@@ -232,13 +232,13 @@
 
 // // Arrays
 // const boxes = document.querySelectorAll('.box');
- 
+
 // es5
 // var boxesArr5 = Array.prototype.slice.call(boxes);   // this gives us an array to use the array method
 // boxesArr5.forEach(function(cur) {  
 // 	cur.style.backgroundColor = 'dodgerblue';
 // });
- 
+
 
 //es6
 // boxesArr6 = Array.from(boxes);
@@ -445,7 +445,7 @@
 // }
 
 // Person5.prototype.calculateAge = function() {
-// 	var age = new Date().getFullYear - this.year;
+// 	var age = new Date().getFullYear() - this.year;
 // 	console.log(age);
 // }
 
@@ -471,3 +471,200 @@
 
 // var john6 = new Person6('John', 1995, 'teacher');
 // Person6.greeting();
+
+// Classes with subclasses
+// ES5
+// var Person5 = function(name, yearOfBirth, job) {
+// 	this.name = name;
+// 	this.yearOfBirth = yearOfBirth;
+// 	this.job = job;
+// };
+
+// Person5.prototype.calculateAge = function() {
+// 	var age = new Date().getFullYear() - this.yearOfBirth;
+// 	console.log(age);
+// };
+
+// var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+// 	Person5.call(this, name, yearOfBirth, job);
+// 	this.olympicGames = olympicGames;
+// 	this.medals = medals;
+// };
+
+// Athlete5.prototype = Object.create(Person5.prototype);
+
+// Athlete5.prototype.wonMedal = function() {
+// 	this.medals++;
+// 	console.log(this.medals);
+// };
+
+// var johnAthlete5 = new Athlete5('john', 1990, 'swimmer', 3, 10);
+
+// johnAthlete5.calculateAge();
+// johnAthlete5.wonMedal();
+
+
+//es6 - subclasses
+// class Person6 {
+// 	constructor(name, yearOfBirth,job) {
+// 		this.name = name;
+// 		this.yearOfBirth = yearOfBirth;
+// 		this.job = job;
+// 	}
+
+// 	calculateAge() {
+// 		var age = new Date().getFullYear() - this.yearOfBirth;
+// 		console.log(age);
+// 	}
+// }
+
+// class Athlete6 extends Person6 {
+// 	constructor(name, yearOfBirth, job, olympicGames, medals) {
+// 		super(name, yearOfBirth, job);
+// 		this.olympicGames = olympicGames;
+// 		this.medals = medals;
+// 	}
+
+// 	wonMedal() {
+// 		this.medals++;
+// 		console.log(this.medals);
+// 	}
+// }
+
+// const johnAthlete6 = new Athlete6('john', 1990, 'swimmer', 3, 10);
+// johnAthlete6.calculateAge();
+// johnAthlete6.wonMedal();
+
+
+/////////////////////////////////
+// CODING CHALLENGE
+/*
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+All the report data should be printed to the console.
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+*/
+
+/*jshint esversion: 6 */
+
+class Element {   // element superclass
+	constructor(name, buildYear) {
+		this.name = name;
+		this.buildYear = buildYear;
+	}
+}
+
+class Park extends Element {   //subclass
+	constructor(name, buildYear, area, numTrees) {
+		super(name, buildYear);
+		this.area = area;  //km2
+		this.numTrees = numTrees;
+	}
+
+	treeDensity() {
+		const density = this.numTrees / this.area;
+		console.log(`${this.name} has a tree denisty of ${density} tress per square km.`);
+	}
+}
+
+class Street extends Element {
+	constructor(name, buildYear, length, size = 3) {
+		super(name, buildYear);
+		this.length = length;
+		this.size = size;
+	}
+
+	classifyStreet() {
+		const classification = new Map();
+		classification.set(1, 'tiny');
+		classification.set(2, 'small');
+		classification.set(3, 'normal');   //want 3 to be the default
+		classification.set(4, 'big');
+		classification.set(5, 'huge');
+		console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)}`);
+	}
+}
+
+const allParks = [
+new Park('Green Park', 1987, 0.2, 215),
+new Park('national Park', 1894, 2.9, 3541), 
+new Park('Oak Park', 1953, 0.4, 949)
+];
+
+const allStreets = [
+new Street('Ocean Ave', 1999, 1.1, 4), 
+new Street('evergreen st', 2008, 2.7, 2), 
+new Street('4th street', 2015, 0.8), 
+new Street('Sunset blvd', 1982, 2.5, 5)
+];
+
+
+function calc(array) {
+	// we are adding together all of the values of the array
+
+	// const sum = array.reduce((previous, current, index) => previous + current, 0); Jonas soln
+	const sum = array.reduce((previous, current) => previous + current); //this is a QA improvement
+	// 3, 5, 6
+
+	// returns sum and the average age all at once
+	return [sum, sum / array.length];
+
+
+}
+
+
+
+function reportParks(p) {
+	console.log(`---------------Parks Report--------------`);
+
+	// Denisty
+	p.forEach(cur => cur.treeDensity());
+
+	// Average age
+	const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+	const [totalAge, avgAge] = calc(ages);
+	console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+
+
+	// Which park has more than 1000 trees
+	// const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);  //returns an array 
+	// console.log(`${p[i].name} has more than 1000 trees`);
+	for(const cur of p){   //this is from the QA, using a for of loop
+		if(cur.numTrees > 1000){
+			console.log(`${cur.name} has more than 1000 trees!`);
+		}
+	}
+}
+
+function reportStreets(s) {
+	console.log(`---------------Street Report--------------`);
+
+	//total and avg length of the towns streets
+	const [totalLength, avgLength] = calc(s.map(el => el.length));   //using destructuring to get the lengths
+	console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+
+	// classify sizes
+	s.forEach(el => el.classifyStreet());
+
+}
+
+
+reportParks(allParks);
+reportStreets(allStreets);
+
+
+
+
+
+
+
+
+
